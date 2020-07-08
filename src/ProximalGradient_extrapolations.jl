c1d9dda03e70ca6d386801822aecd9c3314bac27
Gilles Bareilles <gilles.bareilles@univ-grenoble-alpes.com>
2020-07-08 14:18:02 +0200

    add T2, MFISTA

--- src/ProximalGradient_extrapolations.jl
+++ src/ProximalGradient_extrapolations.jl
@@ -156,14 +156,60 @@ end
 #
 ## T2
 #
-struct Test2ProxGrad <: ProxGradExtrapolation end
-mutable struct Test2ProxGradState <: ProxGradExtrapolationState end
+@with_kw struct Test2ProxGrad <: ProxGradExtrapolation
+    acceleration::AcceleratedProxGrad = AcceleratedProxGrad()
+end
+mutable struct Test2ProxGradState{Tx} <: ProxGradExtrapolationState
+    M_previousit::Manifold
+    acceleration_state::AcceleratedProxGradState{Tx}
+    candidate_pg::Tx
+    candidate_apg::Tx
+end
 
-extrapolation_state(::Test2ProxGrad, x, g) = Test2ProxGradState()
-function extrapolation!(::Test2ProxGrad, state, extrastate, pb)
-    state.temp .= state.x .- state.γ .* state.∇f_x
-    M = prox_αg!(pb, state.x, state.temp, state.γ)
-    state.M = M
+function extrapolation_state(o::Test2ProxGrad, x, g::Regularizer)
+    return Test2ProxGradState(
+        wholespace_manifold(g, x),
+        extrapolation_state(o.acceleration, x, g),
+        zero(x),
+        zero(x),
+    )
+end
+function extrapolation!(o::Test2ProxGrad, pgstate, extrastate, pb)
+    @unpack p, q, r = o.acceleration
+    accel_state = extrastate.acceleration_state
+
+
+    ## Compute proximal gradient iterate
+    pgstate.temp .= pgstate.x .- pgstate.γ .* pgstate.∇f_x
+    M_accelerated = prox_αg!(pb, accel_state.y, pgstate.temp, pgstate.γ)
+
+    t_next = (p + sqrt(q + r * accel_state.t^2)) / 2
+
+    extrastate.candidate_pg .= accel_state.y
+    extrastate.candidate_apg .= accel_state.y .+ (accel_state.t - 1) / t_next * (accel_state.y .- accel_state.y_old)
+
+    # Compute image of candidates by PG, only manifold is relevant.
+    ## 1. proxgrad
+    pgstate.temp .= extrastate.candidate_pg .- pgstate.γ .* ∇f(pb, extrastate.candidate_pg)
+    M_PG_pg = prox_αg!(pb, pgstate.temp, pgstate.temp, pgstate.γ)
+
+    pgstate.temp .= extrastate.candidate_apg .- pgstate.γ .* ∇f(pb, extrastate.candidate_apg)
+    M_PG_apg = prox_αg!(pb, pgstate.temp, pgstate.temp, pgstate.γ)
+
+    ## Here, if apg has less structure than pg (in image by proxgrad), no acceleration
+    if !(M_PG_pg < M_PG_apg)
+        pgstate.x .= extrastate.candidate_apg
+        pgstate.M = M_PG_pg
+    else
+        pgstate.x .= extrastate.candidate_pg
+        pgstate.M = M_PG_apg
+    end
+
+    extrastate.M_previousit = pgstate.M
+
+    accel_state.t = t_next
+    accel_state.y_old .= accel_state.y
+
     return
 end
 
@@ -187,9 +233,16 @@ mutable struct MFISTAState{Tx} <: ProxGradExtrapolationState
 end
 
 function extrapolation_state(o::MFISTA, x, g::Regularizer)
-    return MFISTAState(extrapolation_state(o.acceleration, x, g), zero(x), zero(x))
+    return MFISTAState(extrapolation_state(o.acceleration, x, g), copy(x), copy(x))
 end
 
+"""
+    extrapolation!(o::MFISTA, pgstate, extrastate, pb)
+
+MFISTA algorithm.
+
+NOTE: implementaiton might be improved.
+"""
 function extrapolation!(o::MFISTA, pgstate, extrastate, pb)
     @unpack p, q, r = o.acceleration
     accel_state = extrastate.acceleration_state
@@ -201,22 +254,24 @@ function extrapolation!(o::MFISTA, pgstate, extrastate, pb)
     t_next = (p + sqrt(q + r * accel_state.t^2)) / 2
 
 
-    if F(pb, accel_state.y) < F(pb, extrastate.z_old)
+    if F(pb, accel_state.y) <= F(pb, extrastate.z_old)
         extrastate.z .= accel_state.y
     else
-        extrastate.z = extrastate.z_old
+        extrastate.z .= extrastate.z_old
     end
 
-    extrastate.z_old = extrastate.z
-
     pgstate.x .=
-        extrastate.z +
-        accel_state.t / t_next * (accel_state.y - extrastate.z) +
-        (accel_state.t - 1) / t_next * (extrastate.z - extrastate.z_old)
+        extrastate.z .+
+        accel_state.t / t_next .* (accel_state.y .- extrastate.z) .+
+        (accel_state.t - 1) / t_next .* (extrastate.z .- extrastate.z_old)
     pgstate.M = M
+
+    extrastate.z_old .= extrastate.z
+    accel_state.t = t_next
+
     return
 end
 
 function Base.show(io::IO, ::MFISTA)
-    return print(io, " - Restarted Acc.")
+    return print(io, "MFISTA")
 end
